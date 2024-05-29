e71ec4a91022df5898ccfafc8bfc09b125b2d1e4
Rizal Solihin <ripisopol@gmail.com>
2024-05-29 18:29:24 +0700

    init

---
+++ src/main/java/com/learn/sayur/product/Product.java
@@ -0,0 +1,4 @@
+package com.learn.sayur.product;
+
+public class Product {
+}
---
+++ src/test/java/com/learn/sayur/SayurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.sayur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SayurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
